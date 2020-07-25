a2682243b55921bff1119920d6e7379d7922ef9d
yuchen <yl6448@nyu.edu>
2020-07-25 14:21:01 -0700

    Add calculate mathod

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,119 @@
 <template>
   <div id="app">
-    <NumberDisplay :number="number" ></NumberDisplay>
-    <FunctionButton 
-      v-for="button in buttons" 
-      :symbol="button"
-      :key="button"
-      @click="onClick"
-    ></FunctionButton>
-
+    <NumberDisplay :number="number"></NumberDisplay>
+    <FunctionButton v-for="button in buttons" :symbol="button" :key="button" @click="onClick"></FunctionButton>
   </div>
 </template>
 
 <script>
+import NumberDisplay from "./components/NumberDisplay";
+import FunctionButton from "./components/FunctionButton";
 
-import NumberDisplay from './components/NumberDisplay';
-import FunctionButton from './components/FunctionButton';
+const reg = /[+-/%x]/;
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     NumberDisplay,
-    FunctionButton
+    FunctionButton,
   },
-  data: function() {
+  data: function () {
     return {
-      number: '0',
+      number: "0",
       buttons: [
         '(',')', '%', 'AC',
         '7', '8', '9', '÷',
         '4', '5', '6', 'x',
         '1', '2', '3', '-',
         '0', '.', '=', '+'
-        ]
-    }
+        ],
+    };
   },
   methods: {
-    onClick: function(symbol) {
+    onClick: function (symbol) {
+      switch (symbol) {
+        case "AC":
+          this.number = "0";
+          break;
+        case "=":
+          this.calculate();
+          break;
+        default:
+          this.number = this.number === "0" ? symbol : this.number + symbol;
+      }
+    },
+    calculate: function () {
+      const input = this.number;
+      const validOperators = ["+", "-", "%", "x", "/"];
+      const valueStack = [];
+      const operatorStack = [];
+      for (const char of input.split(/[+-]/)) {
+        console.log(char);
+        if (validOperators.includes(char)) {
+          operatorStack.push(char);
+        } else if (char === ")") {
+          const second = valueStack.pop();
+          const first = valueStack.pop();
+          const operator = operatorStack.pop();
+          valueStack.push(this.getValue(first, second, operator));
+        } else if (char !== "(") {
+          valueStack.push(char);
+        }
+      }
 
-      console.log(symbol);
+      const second = valueStack.pop();
+      const first = valueStack.pop();
+      const operator = operatorStack.pop();
+      valueStack.push(this.getValue(first, second, operator));
+      this.number = valueStack.pop().toString();
+    },
+    getValue(first, second, operator) {
+      first = parseInt(first);
+      second = parseInt(second);
+      switch (operator) {
+        case "+":
+          return first + second;
+        case "-":
+          return first - second;
+        case "%":
+          return first % second;
+        case "x":
+          return first * second;
+        case "/":
+          return first / second;
+      }
+    },
+    parseInput: function(input) {
+      const result = [];
+      let string = '';
+      for(let i = 0; i < input.length; i++) {
+        const current = input.strAt(i);
+        if(current.test(reg)) {
+          result.push(string);
+          result.push(current);
+          string = '';
+        }
+        else if(i === input.length - 1) {
+          string += current;
+          result.push(string);
+        }
+        else {
+          string += current;
+        }
+      }
+      return result;
     }
+  },
+  test: function() {
+    return 1;
   }
-}
+};
 </script>
 
 <style>
 @font-face {
-  font-family: 'MyFont';
-  src: url('./assets/roboto.ttf');
+  font-family: "MyFont";
+  src: url("./assets/roboto.ttf");
 }
 #app {
   display: grid;
